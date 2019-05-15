7d30a22fe0ca69d3ac35df83bfe9b7b2386bb7fa
Abhinash1994 <pandit.bechu@gmail.com>
2019-05-16 00:19:22 +0530

    build

---
+++ client/build/precache-manifest.99917bc4efad9651b079b8db8a6809dd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fb77e8d52ed66174bc48",
+    "url": "/static/css/main.89fa63ed.chunk.css"
+  },
+  {
+    "revision": "fb77e8d52ed66174bc48",
+    "url": "/static/js/main.38b0e4ba.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "275cd78a3244032cd438",
+    "url": "/static/js/2.88c096a4.chunk.js"
+  },
+  {
+    "revision": "bd5b0e6320b8509df9f81cb2753763f6",
+    "url": "/index.html"
+  }
+];
